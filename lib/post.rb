cde36fc1e13e9ce8540b41c535b0af4f69c691dd
bradc1993 <bradc1993@gmail.com>
2020-06-07 15:50:48 -0500

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,10 @@
+class Post
+
+    attr_accessor :title, :author
+
+    def initialize
+        @title = title
+        self.author = author
+    end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,9 @@
+class Song
+
+    attr_accessor :title, :artist
+
+    def initialize
+        self.artist = artist
+    end
+    
+end
